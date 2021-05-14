f6582a59a4f0f4971263004f18daaad01a9d1ab6
Victor Mendele <v-mendele@logitud.fr>
2021-05-14 23:52:14 +0200

    try to fix coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const baseConfig = require('./jest.config.base')
 
 module.exports = {
     ...baseConfig,
-    projects: [ '<rootDir>/packages/*/jest.config.js' ],
+    projects: [ 'jest.config.js' ],
     coverageDirectory: '<rootDir>/coverage/',
     collectCoverageFrom: [
         '<rootDir>/packages/*/src/**/*.ts'
