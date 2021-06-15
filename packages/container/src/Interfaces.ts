09f83ab945ee5bb3d1df4c0e2cefc3ab5383bdfc
Victor Mendele <victor.mendele68@gmail.com>
2021-06-15 19:51:59 +0200

    add comments for interfaces

--- packages/container/src/Interfaces.ts
+++ packages/container/src/Interfaces.ts
@@ -1,8 +1,32 @@
+/**
+ * Manage your application dependencies
+ */
 export interface ContainerInterface {
+    /**
+     * Get a sync service with its uniq identifier.
+     *
+     * @param key
+     */
     get<T>(key: ServiceKey): T
+
+    /**
+     * Get an async service with its uniq identifier.
+     * @param key
+     */
     getAsync<T>(key: ServiceKey): Promise<T>
 
+    /**
+     * Return true if there is a sync service matching the given key; otherwise false.
+     *
+     * @param key
+     */
     has(key: ServiceKey): boolean
+
+    /**
+     * Return true if there is an async service matching the given key; otherwise false.
+     *
+     * @param key
+     */
     hasAsync(key: ServiceKey): boolean
 }
 
@@ -12,11 +36,44 @@ export type AsyncServiceFactory<TService> = (provider: AsyncServiceProviderInter
 export type ServiceFactory<TService> = AsyncServiceFactory<TService> | SyncServiceFactory<TService>
 export type ServiceConstructor<TService> = { new(...args: any[]): TService }
 
+/**
+ * Helps container creation.
+ */
 export interface ContainerBuilderInterface {
+    /**
+     * Add a new sync factory to the container.
+     * The services registered with this method can only access sync services.
+     *
+     * @param key A uniq key to identify your service
+     * @param factory A function creating your service instance
+     * @param lifeCycle The lifecycle of your service
+     */
     addFactory<TService>(key: ServiceKey, factory: SyncServiceFactory<TService>, lifeCycle?: LifeCycle): this
+
+    /**
+     * Add a new sync constructor to the container.
+     * This function is the same as `addFactory` but for constructor.
+     * The services registered with this method can only access sync services.
+     *
+     * @param key A uniq key to identify your service.
+     * @param constructor Your service's constructor
+     * @param lifeCycle The lifecycle of your service
+     */
     addConstructor<TConstructor>(key: ServiceKey, constructor: ServiceConstructor<TConstructor>, lifeCycle?: LifeCycle): this
+
+    /**
+     * Add a new async factory to the container.
+     * The services registered with this method can access both sync and async services.
+     *
+     * @param key
+     * @param factory
+     * @param lifeCycle
+     */
     addAsyncFactory<TService>(key: ServiceKey, factory: AsyncServiceFactory<Promise<TService>>, lifeCycle?: LifeCycle): this
 
+    /**
+     * Create a new DI container from your current configuration.
+     */
     build(): ContainerInterface
 }
 
@@ -35,12 +92,18 @@ export enum LifeCycle {
     Singleton,
 }
 
+/**
+ * Thrown when you add an already registered service.
+ */
 export class ServiceAlreadyRegisteredError extends Error {
     constructor(serviceKey: ServiceKey) {
         super(`Service with key "${serviceKey}" was already registered.`);
     }
 }
 
+/**
+ * Thrown when you try to access an non-existing service.
+ */
 export class ServiceNotFoundError extends Error {
     constructor(serviceKey: ServiceKey) {
         super(`No service matching key "${typeof serviceKey === 'symbol' ? serviceKey.toString() : serviceKey}" found.`);
