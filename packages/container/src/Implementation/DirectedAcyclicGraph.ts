b7c90c40109ad6e9df01327779ff7d798869dc40
Victor Mendele <v-mendele@logitud.fr>
2021-08-30 16:22:35 +0200

    add comments in graph file

--- packages/container/src/Implementation/DirectedAcyclicGraph.ts
+++ packages/container/src/Implementation/DirectedAcyclicGraph.ts
@@ -1,16 +1,34 @@
-type Vertex<Key, Value> = {
+/**
+ * The representation of a vertex in the graph.
+ * @see DirectedAcyclicGraph
+ */
+export type Vertex<Key, Value> = {
     key: Key,
     value: Value
     neighbours: Key[]
 }
-type VisitVertex<Key, Value> = (vertex: Vertex<Key, Value>) => void
 
+/**
+ * A visit vertex function type.
+ * @see DirectedAcyclicGraph.traverse
+ */
+export type VisitVertex<Key, Value> = (vertex: Vertex<Key, Value>) => void
+
+/**
+ * Thrown when there is no vertex matching the given key.
+ * @see DirectedAcyclicGraph.getVertex
+ * @see DirectedAcyclicGraph.getVertexStrict
+ */
 export class NoVertexError<Key> extends Error {
     constructor(public readonly key: Key) {
         super(`There is no vertex matching key "${key}".`)
     }
 }
 
+/**
+ * Thrown when the given neighbours doesn't exist.
+ * @see DirectedAcyclicGraph.addVertex
+ */
 export class NoNeighbourError<Key> extends Error {
     constructor(
         public readonly parent: Key,
@@ -20,9 +38,21 @@ export class NoNeighbourError<Key> extends Error {
     }
 }
 
+/**
+ * A simple directed acyclic graph implementation.
+ * This structure is used for storing services and their dependencies.
+ */
 export class DirectedAcyclicGraph<Key, Value> {
     private vertices: Vertex<Key, Value>[] = []
 
+    /**
+     * Add a new vertex into the graph.
+     * This method throws if the given neighbours are not in the graph.
+     *
+     * @param key
+     * @param value
+     * @param neighbours
+     */
     addVertex(key: Key, value: Value, neighbours: Key[] = []): DirectedAcyclicGraph<Key, Value> {
         for (const neighbour of neighbours) {
             if (!this.isVertexExisting(neighbour)) {
@@ -38,15 +68,32 @@ export class DirectedAcyclicGraph<Key, Value> {
         return this
     }
 
+    /**
+     * Return true if there is an existing vertex for the given key.
+     *
+     * @param key
+     */
     isVertexExisting(key: Key): boolean {
         return !!this.getVertex(key)
     }
 
+    /**
+     * Return a vertex from its key.
+     * If there is no vertex matching the given key then `undefined` is returned.
+     *
+     * @param key
+     */
     getVertex(key: Key): Vertex<Key, Value> | undefined {
         return this.vertices.find(vertex =>
             vertex.key === key)
     }
 
+    /**
+     * A stricter version a `getVertex`.
+     * This method throws when there is no vertex found.
+     *
+     * @param key
+     */
     getVertexStrict(key: Key): Vertex<Key, Value> {
         const vertex = this.getVertex(key)
 
@@ -56,12 +103,24 @@ export class DirectedAcyclicGraph<Key, Value> {
         return vertex
     }
 
+    /**
+     * Return true if two vertices are connected.
+     *
+     * @param key1
+     * @param key2
+     */
     areConnected(key1: Key, key2: Key): boolean {
         const vertex = this.getVertexStrict(key1)
 
         return !!vertex.neighbours.find(key => key === key2)
     }
 
+    /**
+     * Traverse the graph.
+     *
+     * @param key
+     * @param visit
+     */
     traverse(key: Key, visit: VisitVertex<Key, Value>): void {
         const vertex = this.getVertexStrict(key)
 
